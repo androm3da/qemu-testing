0f134be0168e99a9b6b107022f9bb91ca8a36c49
Brian Cain <brian.cain@oss.qualcomm.com>
2025-07-25 23:04:24 -0500

    initial

---
+++ sdk_examples/stubs/src/subsys_linux.c
@@ -0,0 +1,9 @@
+/*
+ * Stub implementation of subsys.c for Linux builds
+ * Provides no-op subsys_enable() function
+ */
+#include "subsys.h"
+
+void subsys_enable() {
+    // No-op for Linux builds - hardware subsystem control not needed
+}
